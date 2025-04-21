ee44bc25f721070213d84468e26101cf74d76e1b
Mateo Baccillere <mateobaccillere1@gmail.com>
2025-04-21 15:15:07 -0300

    Add services to Maven Repository

---
+++ backend/businessdomain/data-calculator-service/data-calculator-service/src/main/java/com/technovision/app/data_calculator_service/DataCalculatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.technovision.app.data_calculator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCalculatorServiceApplication.class, args);
+	}
+
+}
---
+++ backend/businessdomain/data-calculator-service/data-calculator-service/src/test/java/com/technovision/app/data_calculator_service/DataCalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technovision.app.data_calculator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
